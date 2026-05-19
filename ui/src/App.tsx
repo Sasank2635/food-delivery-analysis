657d8509e3aeec6f21a19da85a5f0458f0b88a90
Sasanka Sekhar Upadhyaya <sasankasekhar.upadhyaya@cozentus.com>
2026-05-19 12:31:44 +0530

    feat: wire up routing and complete full dashboard

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,3 +1,13 @@
+import { BrowserRouter } from 'react-router-dom'
+import { AnalysisProvider } from './state/AnalysisContext'
+import { AppRoutes } from './routes'
+
 export default function App() {
-  return <div>Loading...</div>
+  return (
+    <BrowserRouter>
+      <AnalysisProvider>
+        <AppRoutes />
+      </AnalysisProvider>
+    </BrowserRouter>
+  )
 }
